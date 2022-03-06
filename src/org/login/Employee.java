94a37a7434fa21995d0a87ada8fc8201cd69cf57
koorinath <koorinath@gmail.com>
2022-03-06 16:39:33 +0530

    Dfg

---
+++ src/org/login/Employee.java
@@ -0,0 +1,18 @@
+package org.login;
+
+public class Employee {
+	
+	private void empId() {
+		System.out.println("test1");
+	}
+	
+	private void empId1() {
+		System.out.println("test1");
+	}
+	
+	private void empId2() {
+		System.out.println("test1");
+	}
+	
+
+}
